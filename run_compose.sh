bd1c36fe88a1d6b5e48ce3a2ef86bd2e035ababa
qianyun <yun.qian@scmify.com>
2025-03-19 15:50:03 +0800

    初始化-init

---
+++ run_compose.sh
@@ -0,0 +1,16 @@
+# 运行准备脚本
+SYSTEM=$(uname -s)
+source ./.env
+echo "Environment: $GLOBAL_ENV"
+FileLocation="./"
+PROJECT_NAME=scp-ai-$(echo $GLOBAL_ENV | cut -d '.' -f 1)
+if [ "$SYSTEM" = "Darwin" ];then
+  ReplaceCommand="sed_-i_\'\'"
+  ./set_env_variable.sh $ReplaceCommand $FileLocation
+  docker-compose -p ${PROJECT_NAME} -f ./after-set-variable-local.yml --env-file ./.env up -d --remove-orphans
+else if [ "$SYSTEM" = "Linux" ];then
+    ReplaceCommand='sed_-i'
+    ./set_env_variable.sh $ReplaceCommand $FileLocation
+     docker-compose -p ${PROJECT_NAME} -f ./after-set-variable.yml --env-file ./.env up -d --remove-orphans
+    fi
+fi
