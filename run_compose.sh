8ba7168b122538d6e280ecb9fc8a258de07c306a
qianyun <yun.qian@scmify.com>
2025-03-24 15:46:11 +0800

    初始化-init

--- run_compose.sh
+++ run_compose.sh
@@ -7,10 +7,10 @@ PROJECT_NAME=scp-ai-$(echo $GLOBAL_ENV | cut -d '.' -f 1)
 if [ "$SYSTEM" = "Darwin" ];then
   ReplaceCommand="sed_-i_\'\'"
   ./set_env_variable.sh $ReplaceCommand $FileLocation
-  docker-compose -p ${PROJECT_NAME} -f ./after-set-variable.yml --env-file ./.env up -d --remove-orphans
+  docker-compose -p ${PROJECT_NAME} -f ./after-set-variable.yml --env-file ./.env up --gpus all  -d --remove-orphans
 else if [ "$SYSTEM" = "Linux" ];then
     ReplaceCommand='sed_-i'
     ./set_env_variable.sh $ReplaceCommand $FileLocation
-     docker-compose -p ${PROJECT_NAME} -f ./after-set-variable.yml --env-file ./.env up -d --remove-orphans
+     docker-compose -p ${PROJECT_NAME} -f ./after-set-variable.yml --env-file ./.env up --gpus all -d --remove-orphans
     fi
 fi
