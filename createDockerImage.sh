cda33832915544014bdd12ecce93d4216f24d625
qianyun <yun.qian@scmify.com>
2025-05-13 14:22:36 +0800

    去除无关文件

--- createDockerImage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# 定义Dockerfile文件的路径
-dockerfile_path1="./scp-go/Dockerfile"
-dockerfile_path2="./scp-ui/Dockerfile"
-
-# 执行docker build命令
-docker build -f $dockerfile_path1 -t scp-go .
-docker build -f $dockerfile_path2 -t scp-ui .
